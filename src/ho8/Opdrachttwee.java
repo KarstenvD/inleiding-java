745e0ec9f3f914bc41e592fba434a77f791820c0
KarstenvD <karstenprive04@gmail.com>
2020-10-29 13:39:05 +0100

    KarstenvD

---
+++ src/ho8/Opdrachttwee.java
@@ -0,0 +1,27 @@
+package ho8;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Opdrachttwee extends Applet {
+    TextField tekstvak;
+    Label label;
+    String s;
+    public void init(){
+        label = new Label("typ iets in het textvakje");
+        tekstvak = new TextField("klik op mij",20);
+        tekstvak.addActionListener(new TekstvakListener());
+        add (tekstvak);
+        add (label);
+    }
+    public void paint(Graphics g) {
+        g.drawString(s,50,50);
+    }
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            s = tekstvak.getText();
+            repaint();
+        }
+    }
+}
