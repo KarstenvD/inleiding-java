7720256d230188c83cb08a856a1d64322a0d4236
KarstenvD <karstenprive04@gmail.com>
2020-10-29 14:22:00 +0100

    KarstenvD

--- src/ho8/Opdrachttwee.java
+++ src/ho8/Opdrachttwee.java
@@ -8,12 +8,16 @@ public class Opdrachttwee extends Applet {
     TextField tekstvak;
     Label label;
     String s;
+    Button reset;
     public void init(){
+        reset = new Button("reset text");
+        reset.addActionListener(new TekstvakListener.KnopListenerr());
         label = new Label("typ iets in het textvakje");
         tekstvak = new TextField("klik op mij",20);
         tekstvak.addActionListener(new TekstvakListener());
         add (tekstvak);
         add (label);
+        add (reset);
     }
     public void paint(Graphics g) {
         g.drawString(s,50,50);
@@ -22,6 +26,12 @@ public class Opdrachttwee extends Applet {
         public void actionPerformed(ActionEvent e) {
             s = tekstvak.getText();
             repaint();
+        }
+        class KnopListenerr implements ActionListener {
+            public void actionPerformed(ActionEvent e) {
+                s = "niks";
+
+            }
         }
     }
 }
