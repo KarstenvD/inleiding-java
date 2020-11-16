aa239aa9c1f146cd4ae036430e809036c34965bf
KarstenvD <karstenprive04@gmail.com>
2020-11-16 13:47:11 +0100

    KarstenvD

--- src/h11/Opdr115.java
+++ src/h11/Opdr115.java
@@ -9,7 +9,14 @@ public class Opdr115 extends Applet {
     }
     public void paint(Graphics g) {
         int teller;
-        for(teller = 0; teller < 10; teller++) {
+        int x;
+        int y;
+        x = 50;
+        y = 50;
+        for(teller = 0; teller < 5; teller++) {
+            x += 25;
+            y += 25;
+            g.drawRect(x, y,25,25);
         }
     }
 }
