e96ecaf291805113bbfdea82b1fea4a7bf32e366
KarstenvD <karstenprive04@gmail.com>
2020-11-19 10:41:19 +0100

    KarstenvD

--- src/h11/Opdr119.java
+++ src/h11/Opdr119.java
@@ -7,7 +7,20 @@ public class Opdr119 extends Applet {
     public void init() {
 
     }
-    public void paint(Graphics g) {
 
+    public void paint(Graphics g) {
+       for(int kolom = 1; kolom < 8; kolom++) {
+           for(int rij = 1; rij < 8; rij++) {
+               if((kolom + rij) % 2 == 1) {
+                   g.setColor(Color.black);
+                   g.fillRect(50 + kolom*20,50 + rij*20,20,20);
+               }
+               else {
+                   g.setColor(Color.white);
+                   g.fillRect(50 + kolom*20,50 + rij*20,20,20);
+               }
+
+           }
+       }
     }
 }
