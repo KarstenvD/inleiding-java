90baa1e58c398a5c22fa250b7f9a7f9f363df445
KarstenvD <karstenprive04@gmail.com>
2020-12-02 11:51:02 +0100

    KarstenvD

--- src/h12/Opdr124.java
+++ src/h12/Opdr124.java
@@ -9,7 +9,14 @@ public class Opdr124 extends Applet {
     int[] getallen = {100, 300, 452, 213, 60, 23, 439, 975};
     TextField tekstvak;
     boolean gevonden;
+    int gezocht;
+    int text;
+    String awnser;
+    String awnser2;
     public void init() {
+        awnser = "";
+        awnser2 = "";
+        gevonden = false;
         tekstvak = new TextField(20);
         tekstvak.addActionListener(new Tekstvak());
         add(tekstvak);
@@ -20,10 +27,28 @@ public class Opdr124 extends Applet {
             y += 15;
             g.drawString("" + getallen[teller], 50, y);
         }
+        g.drawString("" + awnser + awnser2, 100, 100);
     }
     class Tekstvak implements ActionListener {
         public void actionPerformed(ActionEvent e) {
+            gezocht = Integer.parseInt(tekstvak.getText());
+            gevonden = false;
+            for (int teller = 0; teller < getallen.length; teller++) {
+                if(getallen[teller] == gezocht) {
+                    gevonden = true;
+                    text = teller;
+                }
+            }
+            if(gevonden == true) {
+                awnser = "Het getal is gevonden ";
+                awnser2 = "De index is: " + text;
+            }
+            else {
+                awnser = "Het getal is niet gevonden ";
+                awnser2 = "";
+            }
 
+            repaint();
         }
     }
 }
